90b026c703eec3101a327ae86995a93c7886df04
maro <maro@gmail.com>
2024-11-17 20:45:45 +0200

    commit1

---
+++ Bank/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+   public  class Person
+    {
+        public int nameAccount {  get; set; }
+        public string adress {  get; set; }
+        private decimal balance {  get; set; }
+       
+    }
+}
